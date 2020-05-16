1b48135af34cd4c167e45353231e4840c0ad2ecb
takenokoya <takuya1002@gmail.com>
2020-05-17 02:38:57 +0900

    add WordSynth func

--- lib/word_synth.rb
+++ lib/word_synth.rb
@@ -1,5 +1,13 @@
 class WordSynth
+  def initialize
+    @effects = []
+  end
+
+  def add_effect(effect)
+    @effects << effect
+  end
+
   def play(original_words)
-    # 実装は後ほど
+    @effects.inject(original_words) { |words, effect_proc| effect_proc.call(words) }
   end
 end
